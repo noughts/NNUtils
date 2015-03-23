d86dcd47dd891442cc3d3f2ef2c0b6117489481f
koichi yamamoto <koichi@dividual.jp>
2015-03-23 11:08:53 +0900

    a

---
+++ Pod/Classes/UITextField+NNUtils.h
@@ -0,0 +1,16 @@
+//
+//  UITextField+NNUtils.h
+//  Pods
+//
+//  Created by noughts on 2015/03/23.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (NNUtils)
+
+/// placeholderの色を変更。※placeholderをセットした後に呼んでください
+-(void)setPlaceholderColor:(UIColor*)color;
+
+@end
---
+++ Pod/Classes/UITextField+NNUtils.m
@@ -0,0 +1,24 @@
+//
+//  UITextField+NNUtils.m
+//  Pods
+//
+//  Created by noughts on 2015/03/23.
+//
+//
+
+#import "UITextField+NNUtils.h"
+#import <NBULog.h>
+
+@implementation UITextField (NNUtils)
+
+/// placeholderの色を変更。※placeholderをセットした後に呼んでください
+-(void)setPlaceholderColor:(UIColor*)color{
+	if( !self.placeholder || self.placeholder.length == 0 ){
+		NBULogWarn(@"placeholderを指定した後に呼んでください");
+		return;
+	}
+	self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:@{ NSForegroundColorAttributeName:[UIColor grayColor] }];
+}
+
+
+@end

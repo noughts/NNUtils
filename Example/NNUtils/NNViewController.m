a40a2127c9bcac73e7b5df48a80a26535b4c8b60
koichi yamamoto <koichi@dividual.jp>
2015-04-03 11:46:17 +0900

    a

--- Example/NNUtils/NNViewController.m
+++ Example/NNUtils/NNViewController.m
@@ -10,13 +10,21 @@
 #import <UIImage+NNUtils.h>
 #import <UIDevice+NNUtils.h>
 #import <UIView+NNUtils.h>
+#import <AMPopTip+NNUtils.h>
 
 @implementation NNViewController{
 	__weak IBOutlet UIButton* _btn;
+	__weak IBOutlet UIView* _target_view;
+	AMPopTip* _popTip;
 }
 
 - (void)viewDidLoad{
     [super viewDidLoad];
+	
+	_popTip = [AMPopTip popTip];
+	/// inView:self.navigationController.view であることに注意！
+	[_popTip showText:@"ほげ" direction:AMPopTipDirectionDown maxWidth:0 inView:self.navigationController.view fromBarButtonItem:self.navigationItem.rightBarButtonItem];
+	
 }
 
 @end
--- Pod/Classes/AMPopTip+NNUtils.h
+++ Pod/Classes/AMPopTip+NNUtils.h
@@ -10,10 +10,10 @@
 
 @interface AMPopTip (NNUtils)
 
-/// 指定したUIBarButtonItemからPopTipを表示
+/// 指定したUIBarButtonItemからPopTipを表示(大抵の場合、inViewはUINavigationController.viewを指定する必要があります)
 - (void)showText:(NSString *)text direction:(AMPopTipDirection)direction maxWidth:(CGFloat)maxWidth inView:(UIView *)view fromBarButtonItem:(UIBarButtonItem*)barButtonItem;
 
-/// 指定したUIBarButtonItemからPopTipを表示
+/// 指定したUIBarButtonItemからPopTipを表示(大抵の場合、inViewはUINavigationController.viewを指定する必要があります)
 - (void)showAttributedText:(NSAttributedString *)text direction:(AMPopTipDirection)direction maxWidth:(CGFloat)maxWidth inView:(UIView *)view fromBarButtonItem:(UIBarButtonItem*)barButtonItem;
 
 /// 指定したViewからPopTipを表示

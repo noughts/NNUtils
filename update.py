035998c12f756ee9cf5f350e482537858c75d729
Koichi Yamamoto <noughts@gmail.com>
2015-07-02 13:03:45 +0900

    a

--- update.py
+++ update.py
@@ -1,8 +1,26 @@
+#!/usr/bin/env python
+
 import commands;
-import json;
 import os;
 
-result_str = commands.getoutput("pod ipc spec NNUtils.podspec ")
-json = json.loads( result_str );
-version = json["version"];
-os.system( "git st" );
+
+
+def runCommand( command ):
+	print "$ "+ command;
+	os.system( command );
+	pass
+
+
+def getVersion():
+	import json;
+	projectName = os.getcwd().split("/")[-1];
+	result_str = commands.getoutput("pod ipc spec %s.podspec" % projectName)
+	# print result_str;
+	dict = json.loads( result_str );
+	return dict["version"];
+
+
+version = getVersion();
+runCommand( "git tag "+ version );
+runCommand( "git push --tags" );
+runCommand( "pod repo push noughts" );

d360da91451385820cf0d2108b59e1b5ccfe5191
Koichi Yamamoto <noughts@gmail.com>
2015-08-24 17:58:08 +0900

    a

--- Pod/Classes/UIView+NNAnimation.h
+++ Pod/Classes/UIView+NNAnimation.h
@@ -10,6 +10,32 @@
 
 @interface UIView (NNAnimation)
 
+/// フェードイン
+-(void)fadeIn;
+
+/// フェードアウト
+-(void)fadeOut;
+-(void)fadeOutWithCompletion:(void (^)(BOOL finished))completion;
+
+/// ポップして表示アニメ
+-(void)popInAnime:(float)delay;
+
+/// 微妙に拡大&ポップしながらフェードイン
+-(void)fadePopIn:(float)delay;
+
+/// フェードイン
+-(void)fadeInWithDelay:(float)delay;
+
+/// 微妙に拡大しながらフェードイン
+-(void)fadeInWithScale:(float)delay;
+
+/// フェードアウト
+-(void)fadeOutWithDelay:(float)delay;
+
+/// 微妙に縮小しながらフェードアウト
+-(void)fadeOutWithScale:(float)delay;
+
+
 -(void)startGlowAnimation;
 -(void)stopGlowAnimation;
 
--- Pod/Classes/UIView+NNAnimation.m
+++ Pod/Classes/UIView+NNAnimation.m
@@ -7,8 +7,141 @@
 //
 
 #import "UIView+NNAnimation.h"
+#import "UIView+EasingFunctions.h"
+#import "easing.h"
 
 @implementation UIView (NNAnimation)
+
+
+
+
+/// ポップして表示アニメ
+-(void)popInAnime:(float)delay{
+	self.hidden = NO;
+	self.transform = CGAffineTransformMakeScale(0,0);
+	
+	[UIView animateWithDuration:0.2 delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+		[self setEasingFunction:BackEaseOut forKeyPath:@"transform"];
+		self.transform = CGAffineTransformMakeScale(1.0, 1.0);
+	} completion:^(BOOL finished) {
+		[self removeEasingFunctionForKeyPath:@"transform"];
+	}];
+}
+
+
+/// 微妙に拡大&ポップしながらフェードイン
+-(void)fadePopIn:(float)delay{
+	self.transform = CGAffineTransformMakeScale( 0.9, 0.9 );
+	self.hidden = NO;
+	self.alpha = 0;
+	
+	[UIView animateWithDuration:0.25 delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+		[self setEasingFunction:BackEaseOut forKeyPath:@"transform"];
+		self.transform = CGAffineTransformMakeScale( 1, 1 );
+		self.alpha = 1;
+	} completion:^(BOOL finished) {
+		[self removeEasingFunctionForKeyPath:@"transform"];
+	}];
+}
+
+
+/// フェードイン
+-(void)fadeIn{
+	if( self.alpha == 1 && self.hidden == NO ){
+		return;
+	}
+	self.alpha = 0;
+	self.hidden = NO;
+	[UIView animateWithDuration:0 delay:0 options:(7 << 16) animations:^{
+		self.alpha = 1;
+	} completion:^(BOOL finished) {
+	}];
+}
+
+
+/// delayありフェードイン
+-(void)fadeInWithDelay:(float)delay{
+	self.alpha = 0;
+	self.hidden = NO;
+	[UIView animateWithDuration:0.25 animations:^{
+		self.alpha = 1;
+	}];
+}
+
+/// 微妙に拡大しながらフェードイン
+-(void)fadeInWithScale:(float)delay{
+	self.transform = CGAffineTransformMakeScale( 0.9, 0.9 );
+	self.hidden = NO;
+	self.alpha = 0;
+	
+	[UIView animateWithDuration:0.25 delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+		[self setEasingFunction:QuarticEaseOut forKeyPath:@"transform"];
+		self.transform = CGAffineTransformMakeScale( 1, 1 );
+		self.alpha = 1;
+	} completion:^(BOOL finished) {
+		[self removeEasingFunctionForKeyPath:@"transform"];
+	}];
+}
+
+
+/// フェードアウト
+-(void)fadeOut{
+	[self fadeOutWithCompletion:nil];
+}
+
+/// フェードアウト
+-(void)fadeOutWithCompletion:(void (^)(BOOL finished))completion{
+	// アニメーションが重複するとスキップしてしまうので無視
+	if( [self.layer.animationKeys containsObject:@"opacity"] ){
+		return;
+	}
+	
+	[UIView animateWithDuration:0 delay:0 options:(7 << 16) animations:^{
+		self.alpha = 0;
+	} completion:^(BOOL finished) {
+		self.hidden = YES;
+		if( completion ){
+			completion( finished );
+		}
+	}];
+}
+
+
+/// delayありフェードアウト
+-(void)fadeOutWithDelay:(float)delay{
+	// アニメーションが重複するとスキップしてしまうので無視
+	if( [self.layer.animationKeys containsObject:@"opacity"] ){
+		return;
+	}
+	
+	[UIView animateWithDuration:0.25 delay:delay options:0 animations:^{
+		self.alpha = 0;
+	} completion:^(BOOL finished) {
+		self.hidden = YES;
+	}];
+}
+
+/// 微妙に縮小しながらフェードアウト
+-(void)fadeOutWithScale:(float)delay{
+	[UIView animateWithDuration:0.2 delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
+		[self setEasingFunction:QuarticEaseOut forKeyPath:@"transform"];
+		self.transform = CGAffineTransformMakeScale( 0.9, 0.9 );
+		self.alpha = 0;
+	} completion:^(BOOL finished) {
+		self.hidden = YES;
+		self.alpha = 1;
+		[self removeEasingFunctionForKeyPath:@"transform"];
+	}];
+}
+
+
+
+
+
+
+
+
+
 
 -(void)startGlowAnimation{
 	UIViewAnimationOptions options = UIViewAnimationOptionAutoreverse | UIViewAnimationCurveEaseInOut | UIViewAnimationOptionRepeat | UIViewAnimationOptionAllowUserInteraction;
